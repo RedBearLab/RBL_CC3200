dc956ff39e04b2a5dc233d56b0eca9bf2ce05a95
guohui <xghui999@gmail.com>
2015-01-07 09:59:59 +0800

    CC3200 W2 pins_energia.h

--- Energia/hardware/cc3200/variants/RedBearLab-W2/pins_energia.h
+++ Energia/hardware/cc3200/variants/RedBearLab-W2/pins_energia.h
@@ -42,7 +42,13 @@
 #include "driverlib/adc.h"
 #include "driverlib/pin.h"
 
-static const uint8_t LED = 13;
+#define LED  13
+static const uint8_t RED_LED = 13;
+static const uint8_t GREEN_LED = 13;
+static const uint8_t YELLOW_LED = 13;
+
+static const uint8_t PUSH1 = 20;
+static const uint8_t PUSH2 = 21;
 
 static const uint8_t SDA = 14;
 static const uint8_t SCL = 15;
@@ -71,24 +77,24 @@ const uint16_t digital_pin_to_pin_num[] = {
     PIN_55,         // D1 - IO1
     PIN_61,         // D2 - IO6
     PIN_62,         // D3 - IO7
-    PIN_63,         // D4 - IO8
+    PIN_50,         // D4 - IO0
     PIN_64,         // D5 - IO9
     PIN_17,         // D6 - IO24
-    PIN_03,         // D7 - IO12
-    PIN_04,         // D8 - IO13
-    PIN_15,         // D9 - IO22
-    PIN_08,         // D10 - IO17
-    PIN_16,         // D11 - IO23
-    PIN_20,         // D12 - IO29
-    PIN_50,         // D13 - IO0
+    PIN_18,         // D7 - IO28
+    PIN_20,         // D8 - IO29
+    PIN_16,         // D9 - IO23
+    PIN_15,         // D10 - IO22
+    PIN_04,         // D11 - IO13
+    PIN_08,         // D12 - IO17
+    PIN_53,         // D13 - IO30
     PIN_02,         // D14 - IO11
     PIN_01,         // D15 - IO10
     PIN_57,         // D16/A0 - IO02
     PIN_60,         // D17/A1 - IO05
     PIN_59,         // D18/A2 - IO04
     PIN_58,         // D19/A3 - IO03    
-    PIN_53,         // D20 - IO30
-    PIN_18,         // D21 - IO28
+    PIN_63,         // D20 - IO8
+    PIN_03,         // D21 - IO12
     PIN_07,         // D22 - IO16
     PIN_06,         // D23 - IO15
     PIN_05,         // D24 - IO14
@@ -99,24 +105,24 @@ const uint8_t digital_pin_to_port[] = {
     S0,         // D1 - IO1
     S0,         // D2 - IO6
     S0,         // D3 - IO7
-    S1,         // D4 - IO8
+    S0,         // D4 - IO0    
     S1,         // D5 - IO9
     S3,         // D6 - IO24
-    S1,         // D7 - IO12
-    S1,         // D8 - IO13
-    S2,         // D9 - IO22
-    S2,         // D10 - IO17
-    S2,         // D11 - IO23
-    S3,         // D12 - IO29
-    S0,         // D13 - IO0
+    S3,         // D7 - IO28
+    S3,         // D8 - IO29
+    S2,         // D9 - IO23
+    S2,         // D10 - IO22
+    S1,         // D11 - IO13
+    S2,         // D12 - IO17
+    S3,         // D13 - IO30 
     S1,         // D14 - IO11
     S1,         // D15 - IO10
     S0,         // D16/A0 - IO02
     S0,         // D17/A1 - IO05
     S0,         // D18/A2 - IO04
-    S0,         // D19/A3 - IO03    
-    S3,         // D20 - IO30
-    S3,         // D21 - IO28
+    S0,         // D19/A3 - IO03 
+    S1,         // D20 - IO8    
+    S1,         // D21 - IO12     
     S2,         // D22 - IO16
     S1,         // D23 - IO15
     S1,         // D24 - IO14 
@@ -127,24 +133,24 @@ const uint8_t digital_pin_to_bit_mask[] = {
     BV(1),         // D1 - IO1
     BV(6),         // D2 - IO6
     BV(7),         // D3 - IO7
-    BV(0),         // D4 - IO8
+    BV(0),         // D4 - IO0   
     BV(1),         // D5 - IO9
     BV(0),         // D6 - IO24
-    BV(4),         // D7 - IO12
-    BV(5),         // D8 - IO13
-    BV(6),         // D9 - IO22
-    BV(1),         // D10 - IO17
-    BV(7),         // D11 - IO23
-    BV(5),         // D12 - IO29
-    BV(0),         // D13 - IO0
+    BV(4),         // D7 - IO28
+    BV(5),         // D8 - IO29
+    BV(7),         // D9 - IO23
+    BV(6),         // D10 - IO22
+    BV(5),         // D11 - IO13   
+    BV(1),         // D12 - IO17
+    BV(6),         // D13 - IO30
     BV(3),         // D14 - IO11
     BV(2),         // D15 - IO10
     BV(2),         // D16/A0 - IO02
     BV(5),         // D17/A1 - IO05
     BV(4),         // D18/A2 - IO04
-    BV(3),         // D19/A3 - IO03    
-    BV(6),         // D20 - IO30
-    BV(4),         // D21 - IO28
+    BV(3),         // D19/A3 - IO03 
+    BV(0),         // D20 - IO8       
+    BV(4),         // D21 - IO12
     BV(0),         // D22 - IO16
     BV(7),         // D23 - IO15
     BV(6),         // D24 - IO14
@@ -155,24 +161,24 @@ const uint8_t digital_pin_to_timer[] = {
     NOT_ON_TIMER,         // D1 - IO1
     NOT_ON_TIMER,         // D2 - IO6
     NOT_ON_TIMER,         // D3 - IO7
-    NOT_ON_TIMER,         // D4 - IO8
+    NOT_ON_TIMER,         // D4 - IO0    
     TIMERA2B,         // D5 - IO9
     TIMERA0A,         // D6 - IO24
-    NOT_ON_TIMER,         // D7 - IO12
-    NOT_ON_TIMER,         // D8 - IO13
-    NOT_ON_TIMER,         // D9 - IO22
-    NOT_ON_TIMER,         // D10 - IO17
-    NOT_ON_TIMER,         // D11 - IO23
-    NOT_ON_TIMER,         // D12 - IO29
-    NOT_ON_TIMER,         // D13 - IO0
+    NOT_ON_TIMER,         // D7 - IO28
+    NOT_ON_TIMER,         // D8 - IO29
+    NOT_ON_TIMER,         // D9 - IO23
+    NOT_ON_TIMER,         // D10 - IO22
+    NOT_ON_TIMER,         // D11 - IO13   
+    NOT_ON_TIMER,         // D12 - IO17
+    NOT_ON_TIMER,         // D13 - IO30  
     TIMERA3B,         // D14 - IO11
     TIMERA3A,         // D15 - IO10
     NOT_ON_TIMER,         // D16/A0 - IO02
     NOT_ON_TIMER,         // D17/A1 - IO05
     NOT_ON_TIMER,         // D18/A2 - IO04
-    NOT_ON_TIMER,         // D19/A3 - IO03    
-    NOT_ON_TIMER,         // D20 - IO30
-    NOT_ON_TIMER,         // D21 - IO28
+    NOT_ON_TIMER,         // D19/A3 - IO03  
+    NOT_ON_TIMER,         // D20 - IO8
+    NOT_ON_TIMER,         // D21 - IO12
     NOT_ON_TIMER,         // D22 - IO16
     NOT_ON_TIMER,         // D23 - IO15
     NOT_ON_TIMER,         // D24 - IO14 
